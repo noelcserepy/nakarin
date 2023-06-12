8fc2cf62a40ce92b117a9ea1c8b7979ce1b84647
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2023-06-13 01:18:56 +0200

    Update useMediaQuery.jsx by adding template string in Type checking Media Queries

--- hooks/useMediaQuery.jsx
+++ hooks/useMediaQuery.jsx
@@ -1,22 +1,23 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
-  const twQueries = {
-    "xs": "(max-width: 639px)",
-    "sm": "(min-width: 640px)",
-    "md": "(min-width: 768px)",
-    "lg": "(min-width: 1024px)",
-    "xl": "(min-width: 1280px)",
-    "2xl": "(min-width: 1536px)",
-  } as const;
+const twQueries = {
+  "xs": "(max-width: 639px)",
+  "sm": "(min-width: 640px)",
+  "md": "(min-width: 768px)",
+  "lg": "(min-width: 1024px)",
+  "xl": "(min-width: 1280px)",
+  "2xl": "(min-width: 1536px)",
+} as const;
 
-type TwQueries =keyof typeof twQueries
-type MediaQueries = typeof twQueries[TwQueries]
+type TwQueries = keyof typeof twQueries
+type MediaQueries = `(min-width: ${number}px)` | `(max-width: ${number}px)`
 
 type Breakpoint = TwQueries | MediaQueries
+
 export function useMediaQuery(breakpoint: Breakpoint) {
   const [matches, setMatches] = useState(false);
 
-  let query= breakpoint;
+  let query = breakpoint;
   if (Object.keys(twQueries).find((key) => key === breakpoint)) {
     query = twQueries[breakpoint as TwQueries]
   }
@@ -40,4 +41,3 @@ export function useMediaQuery(breakpoint: Breakpoint) {
 
   return [matches, setMatches];
 }
-
